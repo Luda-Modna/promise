317711064b8580a39589918b21b0872878cf5c9f
Luda Modna <ludmilamodna3@gmail.com>
2025-05-02 18:16:51 +0300

    Added Promisification

--- script.js
+++ script.js
@@ -2,12 +2,44 @@
 
 //promise
 
-const weatherUrl =
-  "https://api.open-meteo.com/v1/forecast?latitude=49.554&longitude=25.5907";
+function promiseCb(resolve, reject) {
+  //resolve('sucess result');
+  reject(new Error("err"));
+}
 
-fetch(weatherUrl)
-  .then((response) => response.json())
+const promise = new Promise(promiseCb);
+
+promise.then((data) => console.log(data)).catch((err) => console.log(err));
+
+// promise - Schroedinger's cat
+
+const schredCat = new Promise((resolve, reject) => {
+  if (Math.random() < 0.5) {
+    resolve("cat is alive");
+  } else {
+    reject(new Error("cat isn't alive "));
+  }
+})
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
 
-console.log("first");
+//Промісифікація
+
+function delay(ms) {
+  const executor = function (res, rej) {
+    if (typeof ms !== "number") {
+      rej(new TypeError("ms is NaN"));
+    }
+    if (ms < 0 || !Number.isInteger(ms)) {
+      rej(new RangeError("ms must be positive integer"));
+    }
+
+    setTimeout(res, ms);
+  };
+
+  return new Promise(executor);
+}
+
+delay(1000)
+  .then(() => console.log("over"))
+  .catch((err) => console.log(err));
